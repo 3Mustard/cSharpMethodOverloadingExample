61ada8306e66f3aa9da0ee005673da071b55c13a
Matthew Cullen <m23.cullen@gmail.com>
2021-06-08 19:50:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LESSON2_ConstructorHandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Owner momo = new Owner("Momo");
+            Cat ownedCat = new Cat("Lennny", momo);
+            Cat unownedCat = new Cat("Buddy");
+        }
+    }
+
+    class Cat
+    {
+        public string Name { get; set; }
+        public Owner Owner { get; set; }
+
+        public Cat(string name)
+        {
+            Name = name;
+            Console.WriteLine($"ADDED CAT: {Name}");
+        }
+
+        public Cat(string name, Owner owner)
+        {
+            Name = name;
+            Owner = owner;
+            Console.WriteLine($"ADDED CAT: {Name}, OWNER: {Owner.Name}");
+        }
+    }
+
+    class Owner
+    {
+        public string Name { get; set; }
+
+        public Owner(string name)
+        {
+            Name = name;
+            Console.WriteLine($"ADDED OWNER: {Name}");
+        }
+    }
+}
